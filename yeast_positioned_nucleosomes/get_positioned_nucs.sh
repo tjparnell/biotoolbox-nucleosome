3f94931b52bf1ebc17d45e1a7517dd361b167af4
Timothy Parnell <parnell.tj@gmail.com>
2019-03-20 13:55:33 -0600

    add terminal nucleosome, emphasize genic nucleosome roles
    This will prioritize terminal and plus1 nucleosome over minus1 and
    minus2, so the numbers in each class will change considerably from
    previous versions. Everything is retained in the unfiltered file so
    that all roles can be checked.

--- yeast_positioned_nucleosomes/get_positioned_nucs.sh
+++ yeast_positioned_nucleosomes/get_positioned_nucs.sh
@@ -5,26 +5,25 @@
 
 # Input database of transcripts and all nucleosomes
 # prepare a SQLite database loaded with modified UTR gene GFF3 with the WT nucleosomes
-# bp_seqfeature_load.pl -c -f -a DBI::SQLite \
-# -d SacCer3_nuc.db \
-# SacCer3_R64_all_genes_NoDubious_UTR_chromo.gff3 
-# nucleosome.gff3 
+# bp_seqfeature_load.pl -N -c -f -a DBI::SQLite -d SacCer3_nuc.db \
+# SacCer3_R64_all_genes_NoDubious_UTR_chromo.gff3 nucleosome.gff3 
 
 # pass the name of the database file and source name of nucleosome data to this script
 DATABASE=$1
 NAME=$2
 
-if [ -e $DATABASE ]
+if [ -z "$DATABASE" ]
 then
-	echo "##### Positioned nucleosome generator #######"
-	echo "using database $DATABASE"
-else
 	echo " Please provide the name of a Bio::DB::SeqFeature::Store database containing"
 	echo " both transcripts and all mapped nucleosomes loaded into it. See documentation."
-	echo " Also provide a source name to the nucleosome"
-	echo " $0 {SacCer3_nuc.db} {name}"
+	echo " Also provide the text for the source_tag (column 3) for the nucleosome gff file"
+	echo
+	echo " $0 <SacCer3_nuc.db> <source_tag>"
 	echo 
 	exit
+else
+	echo "##### Positioned nucleosome generator #######"
+	echo "using database $DATABASE"
 fi
 
 
@@ -35,7 +34,7 @@ echo "####### Collecting Transcripts"
 # transcript for collecting strand-oriented nucleosomes
 
 # two transcript files:
-# 	transcript_list.txt has all transcripts
+# 	transcript_list.txt has all mRNA transcripts
 #	transcript_list_prom500.txt has transcripts with no intersecting transcripts 500 bp upstream
 
 # collect all transcripts
@@ -82,7 +81,13 @@ get_intersecting_features.pl --feature nucleosome:$NAME --in transcript_list.txt
 echo "# collecting plus4 nuc"
 get_intersecting_features.pl --feature nucleosome:$NAME --in transcript_list.txt --start 475 --stop 600 --ref start --out ${NAME}_nuc_plus4
 
+# terminal nucleosome
+echo "# collecting term1 nuc"
+get_intersecting_features.pl --feature nucleosome:$NAME --in transcript_list.txt --start -100 --stop 25 --pos 3 --ref start --out ${NAME}_nuc_term1
+
 # minus 1 nucleosome all genes
+# most minus1 nucleosomes are actually terminal or plus1 nucleosomes, and actual minus1
+# are from larger intergenic regions, so take it from the prom500 list instead
 echo "# collecting all minus1 nuc"
 get_intersecting_features.pl --feature nucleosome:$NAME --in transcript_list.txt --start=-250 --stop=-125 --ref start --out ${NAME}_nuc_minus1
 
@@ -100,7 +105,7 @@ get_intersecting_features.pl --feature nucleosome:$NAME --in transcript_list_pro
 echo
 echo
 echo "#### processing the positioned intersected nucleosome"
-for nuc in nuc_plus1 nuc_plus2 nuc_plus3 nuc_plus4 nuc_minus1 nuc_minus2 
+for nuc in nuc_plus1 nuc_plus2 nuc_plus3 nuc_plus4 nuc_minus1 nuc_minus2 nuc_term1
 do
 	echo
 	echo "## processing $name"
@@ -108,16 +113,20 @@ do
 	
 	# drop anything that doesn't intersect with a nucleosome
 	# even when two nucs intersect, the one with most overlap is reported
-	echo "# removing non-intersecting genes"
+	echo "# removing genes without nucleosomes"
 	manipulate_datasets.pl --func below --index 4 --target 1 --in ${NAME}_${nuc}
 	
+	# discard nucleosomes that barely intersect
+	echo "# removing genes without nucleosomes"
+	manipulate_datasets.pl --func below --index 9 --target 30 --in ${NAME}_${nuc}
+	
 	echo "# applying common value $name"
-	manipulate_datasets.pl --func new --target $nuc --name Location --in ${NAME}_${nuc}
+	manipulate_datasets.pl --func new --target $nuc --name Role --in ${NAME}_${nuc}
 	
 	# reorder columns, I want to keep
 	#	Name (nucleosome) 5
 	#	Type (nucleosome) 6
-	#   Location 10
+	#   Role 10
 	#	Name (transcript) 1
 	#	Strand (transcript) 3
 	#	Distance 8
@@ -133,35 +142,56 @@ do
 	echo "# collecting attributes"
 	get_feature_info.pl --in ${NAME}_${nuc} --attrib chromo,start,stop,score
 	# files should now have :
-	# Name, Type, Location, Transcript_Name, strand, Distance, Overlap, Chromo, Start, Stop, Score
+	# Name, Type, Role, Transcript_Name, strand, Distance, Overlap, Chromo, Start, Stop, Score
+	
+	# reformat score
+	manipulate_datasets.pl --func format --target 2 --place r --index 10 --in ${NAME}_${nuc}.txt
 	
 done
 
-# merge the files into one file
-echo "# joining nucleosome files"
-join_data_file.pl --out ${NAME}_nucs.txt ${NAME}_nuc_plus1 ${NAME}_nuc_plus2 ${NAME}_nuc_plus3 ${NAME}_nuc_plus4 ${NAME}_nuc_minus1 ${NAME}_nuc_minus2 
+# merge the files into one unfiltered file
+# this is a specific order to emphasize the roles, priority to first occurrence
+echo "# joining nucleosome files into unfiltered merged list"
+join_data_file.pl --out ${NAME}_nucs_unfiltered.txt ${NAME}_nuc_plus1 ${NAME}_nuc_plus2 ${NAME}_nuc_plus3 ${NAME}_nuc_plus4 ${NAME}_nuc_term1 ${NAME}_nuc_minus1 ${NAME}_nuc_minus2
 
-# cleaning up and making backup
-rm ${NAME}_nuc_plus1.txt ${NAME}_nuc_plus2.txt ${NAME}_nuc_plus3.txt ${NAME}_nuc_plus4.txt ${NAME}_nuc_minus1.txt ${NAME}_nuc_minus2.txt
-cp ${NAME}_nucs.txt ${NAME}_nucs_unfiltered.txt
+manipulate_datasets.pl --func gsort --in ${NAME}_nucs_unfiltered.txt
+
+
+# prioritize plus1 and terminal nucleosomes over minus1
+# specific order
+echo "# merging and prioritizing intergenic nucleosomes"
+join_data_file.pl --out ${NAME}_intergenic_nucs.txt ${NAME}_nuc_plus1 ${NAME}_nuc_term1 ${NAME}_nuc_minus1 ${NAME}_nuc_minus2.txt
+# discard duplicates, will keep first occurrence, which will be either plus1 or term1
+manipulate_datasets.pl --func duplicate --index 0 --in ${NAME}_intergenic_nucs.txt
+
+
 
 # delete the duplicate nucleosomes
 # first need to perform some sorting
-echo "# sorting nucleosome files"
-# first overlap
+echo "# merging and sorting nucleosomes"
+# merge the remaining nucleosome files with the filtered intergenic nucleosomes
+join_data_file.pl --out ${NAME}_nucs.txt ${NAME}_intergenic_nucs.txt ${NAME}_nuc_plus2.txt ${NAME}_nuc_plus3.txt ${NAME}_nuc_plus4.txt
+# first sort by decreasing overlap amount
 manipulate_datasets.pl --func sort --index 6 --dir d --in ${NAME}_nucs.txt
-# second name
+# second by nucleosome name
 manipulate_datasets.pl --func sort --index 0 --dir i --in ${NAME}_nucs.txt
+
+
 echo "# removing duplicate nucleosomes"
+# this will take the first occurring nucleosome, and discard subsequent 
+# nucleosomes sorted by role, overlap, name in increasing priority
 manipulate_datasets.pl --func duplicate --index 0 --in ${NAME}_nucs.txt
 # re-sort by genomic coordinate
 manipulate_datasets.pl --func gsort --in ${NAME}_nucs.txt
+
+# cleaning up and making backup
+rm ${NAME}_nuc_plus1.txt ${NAME}_nuc_plus2.txt ${NAME}_nuc_plus3.txt ${NAME}_nuc_plus4.txt ${NAME}_nuc_minus1.txt ${NAME}_nuc_minus2.txt ${NAME}_nuc_term1.txt ${NAME}_intergenic_nucs.txt 
 
 # split the file 
 echo "# splitting nucleosome file"
 split_data_file.pl --index 2 ${NAME}_nucs.txt
 
-for nuc in nuc_plus1 nuc_plus2 nuc_plus3 nuc_plus4 nuc_minus1 nuc_minus2
+for nuc in nuc_plus1 nuc_plus2 nuc_plus3 nuc_plus4 nuc_minus1 nuc_minus2 nuc_term1
 do
 	echo "# cleaning up file $name"
 	# change the name
@@ -171,9 +201,6 @@ do
 	manipulate_datasets.pl --func reorder --index 0,1,3,4,7-10 --in ${NAME}_${nuc}.txt
 	# files now will have 
 	# Name, Type, Transcript_Name, Transcript_Strand, Chromo, Start, Stop, Score
-	
-	# reformat score
-	manipulate_datasets.pl --func format --target 2 --place r --index 7 --in ${NAME}_${nuc}.txt
 	
 	# export as bed
 	data2bed.pl --in ${NAME}_${nuc}.txt --strand 3 --score 7 
